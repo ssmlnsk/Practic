261ddab773a92ee2860c97a00cb820b313ff3326
vanga997 <promtman1990@gmail.com>
2022-06-09 12:57:36 +0300

    update 09.06 12:57

--- Запросы/CreateTableRequests.sql
+++ Запросы/CreateTableRequests.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS Requests (
 id_request INT PRIMARY KEY auto_increment,
 number_request VARCHAR(255) NOT NULL,
-date_create DATE NOT NULL,
-time_request TIME NOT NULL,
+date_create VARCHAR(255) NOT NULL,
+time_request VARCHAR(255) NOT NULL,
 number_client INT NOT NULL,
 services VARCHAR(255) NOT NULL,
 status_request VARCHAR(255) NOT NULL,
